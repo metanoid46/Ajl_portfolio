a0f4081a197deb2eb667667659fd09aa9397e0d5
metanoid46 <athuljoseliju123@gmail.com>
2025-07-10 17:02:15 +1000

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base:"/Ajl_portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
